59478e0907b44cab2ce2bd14ac4c67e33ed888f3
Sambit Patnaik <sambitpatnaik987@gmail.com>
2026-05-11 17:18:36 +0530

    praticed string

---
+++ PracticeString.java
@@ -0,0 +1,8 @@
+public class PracticeString {
+
+    public static void main(String[] args) {
+        String name = "Sambit";
+        String nam = "rmv";
+        System.out.println(nam.compareTo(name));
+    }
+}
