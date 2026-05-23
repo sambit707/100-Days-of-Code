3d28493d8464bacc50c884d03f0cad81e2358d69
Sambit Patnaik <sambitpatnaik987@gmail.com>
2026-05-23 22:14:47 +0530

    pratice

---
+++ PracticeString.java
@@ -0,0 +1,41 @@
+/* public class PracticeString {
+
+    public static void main(String[] args) {
+        String str = new String("sambit");
+        System.out.println(str);
+        char target = 'b';
+        int foundIndex = -1;
+        char[] charArray = str.toCharArray();
+    
+        
+
+               
+    }
+} */
+
+   /*  public class PracticeString {
+    
+        public static void main(String[] args) {
+            char ch[] = {'n', 'a', 'v', 'i', 'n'};
+            char target = 'v';
+            String s = new String(ch);
+            for(int i = 0; i<ch.length; i++){
+                if (ch[i] == target) {
+                    System.out.println("Found " + target + " at index " + i);
+                    break;
+                }
+            }
+        }
+    } */
+
+        public class PracticeString {
+        
+            public static void main(String[] args) {
+                String s = "sambit";
+                String d = "dipun";
+                System.out.println(s.charAt(3));
+                System.out.println(s.replace(s, d));
+               //  String name = new String("sambit");
+
+            }
+        }
